80613ec38a5007124cfa999902b32658bb24a0f5
StephenYuan7 <2583242079@qq.com>
2021-05-09 15:06:02 +0800

    测试

--- statement.js
+++ statement.js
@@ -44,3 +44,26 @@ function statement (invoice, plays) {
     result += `You earned ${volumeCredits} credits\n`;
     return result;
 }
+
+console.log(statement(  {
+    "customer": "BigCo",
+    "performances": [
+        {
+            "playID": "hamlet",
+            "audience": 55
+        },
+        {
+            "playID": "as-like",
+            "audience": 35
+        },
+        {
+            "playID": "othello",
+            "audience": 40
+        }
+    ]
+},
+    {
+        "hamlet": {"name": "Hamlet", "type": "tragedy"},
+        "as-like": {"name": "As You Like It", "type": "comedy"},
+        "othello": {"name": "Othello", "type": "tragedy"}
+    }))
