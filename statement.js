2ea74e3abdd29be298874a7684a5bd365b22d491
StephenYuan7 <2583242079@qq.com>
2021-05-09 15:46:16 +0800

    提炼format()

--- statement.js
+++ statement.js
@@ -31,13 +31,16 @@ function statement (invoice, plays) {
         if ("comedy" === playFor(perf).type) result += Math.floor(perf.audience / 5);
         return result;
     }
-    
+
+    function format(aNumber) {
+        return new Intl.NumberFormat("en-US",
+            { style: "currency", currency: "USD",
+                minimumFractionDigits: 2 }).format(aNumber);
+    }
+
     let totalAmount = 0;
     let volumeCredits = 0;
     let result = `Statement for ${invoice.customer}\n`;
-    const format = new Intl.NumberFormat("en-US",
-        { style: "currency", currency: "USD",
-            minimumFractionDigits: 2 }).format;
 
     for (let perf of invoice.performances) {
         volumeCredits += volumeCreditsFor(perf);
