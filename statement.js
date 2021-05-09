a823fcdf75d9562aeea557b022849bc07930eb3d
StephenYuan7 <2583242079@qq.com>
2021-05-09 16:08:20 +0800

    appleSauce返回值修改

--- statement.js
+++ statement.js
@@ -47,14 +47,14 @@ function statement (invoice, plays) {
     }
 
     function appleSauce() {
-        let totalAmount = 0;
+        let result = 0;
         for (let perf of invoice.performances) {
 // print line for this order
             result += ` ${playFor(perf).name}: ${usd(amountFor(perf) / 100)} (${perf.audience} seats)\n`
             ;
-            totalAmount += amountFor(perf);
+            result += amountFor(perf);
         }
-        return totalAmount;
+        return result;
     }
 
     let result = `Statement for ${invoice.customer}\n`;
