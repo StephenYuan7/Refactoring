52974c248f6bd06294bb728b6ec4f5c82a69110a
StephenYuan7 <2583242079@qq.com>
2021-05-09 15:17:05 +0800

    提炼play变量
    先提炼函数，再用Inline去掉变量，最后更改参数名

--- statement.js
+++ statement.js
@@ -28,8 +28,12 @@ function statement (invoice, plays) {
         return result;
     }
 
+    function playFor(aPerformance) {
+        return plays[aPerformance.playID];
+    }
+
     for (let perf of invoice.performances) {
-        const play = plays[perf.playID];
+        const play = playFor(perf);
         let thisAmount = amountFor(play, perf);
 // add volume credits
         volumeCredits += Math.max(perf.audience - 30, 0);
