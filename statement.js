7d4c527721fc56de50c381db36cc4061aee276df
StephenYuan7 <2583242079@qq.com>
2021-05-10 10:15:03 +0800

    搬移playfor函数

--- statement.js
+++ statement.js
@@ -1,39 +1,47 @@
 function statement (invoice, plays) {
     const statementData = {};
-    return renderPlainText(statementData, invoice, plays);
-}
+    statementData.customer = invoice.customer;
+    statementData.performances = invoice.performances.map(enrichPerformance);
+    return renderPlainText(statementData, plays);
 
-function renderPlainText (data, invoice, plays) {
-    function amountFor(perf) {
-        let result = 0;
-        switch (playFor(perf).type) {
-            case "tragedy":
-                result = 40000;
-                if (perf.audience > 30) {
-                    result += 1000 * (perf.audience - 30);
-                }
-                break;
-            case "comedy":
-                result = 30000;
-                if (perf.audience > 20) {
-                    result += 10000 + 500 * (perf.audience - 20);
-                }
-                result += 300 * perf.audience;
-                break;
-            default:
-                throw new Error(`unknown type: ${playFor(perf).type}`);
-        }
+    function enrichPerformance(aPerformance) {
+        const result = Object.assign({}, aPerformance);
+        result.play = playFor(result);
         return result;
     }
 
     function playFor(aPerformance) {
         return plays[aPerformance.playID];
     }
+}
 
-    function volumeCreditsFor(perf) {
+function renderPlainText (data, plays) {
+    function amountFor(aPerformance) {
         let result = 0;
-        result += Math.max(perf.audience - 30, 0);
-        if ("comedy" === playFor(perf).type) result += Math.floor(perf.audience / 5);
+        switch (aPerformance.play.type) {
+            case "tragedy":
+                result = 40000;
+                if (aPerformance.audience > 30) {
+                    result += 1000 * (aPerformance.audience - 30);
+                }
+                break;
+            case "comedy":
+                result = 30000;
+                if (aPerformance.audience > 20) {
+                    result += 10000 + 500 * (aPerformance.audience - 20);
+                }
+                result += 300 * aPerformance.audience;
+                break;
+            default:
+                throw new Error(`unknown type: ${aPerformance.play.type}`);
+        }
+        return result;
+    }
+
+    function volumeCreditsFor(aPerformance) {
+        let result = 0;
+        result += Math.max(aPerformance.audience - 30, 0);
+        if ("comedy" === aPerformance.play.type) result += Math.floor(aPerformance.audience / 5);
         return result;
     }
 
@@ -45,29 +53,26 @@ function renderPlainText (data, invoice, plays) {
 
     function totalVolumeCredits() {
         let volumeCredits = 0;
-        for (let perf of invoice.performances) {
+        for (let perf of data.performances) {
             volumeCredits += volumeCreditsFor(perf);
         }
         return volumeCredits;
     }
 
-    function appleSauce() {
+    function totalAmount() {
         let result = 0;
-        for (let perf of invoice.performances) {
-// print line for this order
-            result += ` ${playFor(perf).name}: ${usd(amountFor(perf) / 100)} (${perf.audience} seats)\n`
-            ;
+        for (let perf of data.performances) {
+            result += amountFor(perf);
         }
         return result;
     }
-
-    let result = `Statement for ${invoice.customer}\n`;
-    for (let perf of invoice.performances) {
+    let result = `Statement for ${data.customer}\n`;
+    for (let perf of data.performances) {
 // print line for this order
-        result += ` ${playFor(perf).name}: ${usd(amountFor(perf) / 100)} (${perf.audience} seats)\n`
+        result += ` ${perf.play.name}: ${usd(amountFor(perf) / 100)} (${perf.audience} seats)\n`
         ;
     }
-    result += `Amount owed is ${usd(appleSauce()/100)}\n`;
+    result += `Amount owed is ${usd(totalAmount()/100)}\n`;
     result += `You earned ${(totalVolumeCredits())} credits\n`;
     return result;
 }
