989725d027ecb9618b06b4b3668118cb89747240
StephenYuan7 <2583242079@qq.com>
2021-05-10 10:21:33 +0800

    搬移观众量积分

--- statement.js
+++ statement.js
@@ -8,6 +8,7 @@ function statement (invoice, plays) {
         const result = Object.assign({}, aPerformance);
         result.play = playFor(result);
         result.amount = amountFor(result);
+        result.volumeCredits = volumeCreditsFor(result);
         return result;
     }
 
@@ -37,9 +38,6 @@ function statement (invoice, plays) {
         return result;
     }
 
-}
-
-function renderPlainText (data) {
     function volumeCreditsFor(aPerformance) {
         let result = 0;
         result += Math.max(aPerformance.audience - 30, 0);
@@ -47,6 +45,9 @@ function renderPlainText (data) {
         return result;
     }
 
+}
+
+function renderPlainText (data) {
     function usd(aNumber) {
         return new Intl.NumberFormat("en-US",
             { style: "currency", currency: "USD",
@@ -56,7 +57,7 @@ function renderPlainText (data) {
     function totalVolumeCredits() {
         let volumeCredits = 0;
         for (let perf of data.performances) {
-            volumeCredits += volumeCreditsFor(perf);
+            volumeCredits += perf.volumeCredits;
         }
         return volumeCredits;
     }
